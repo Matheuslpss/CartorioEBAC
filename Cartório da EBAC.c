2e059c2f2cfca2f633450985a95eb5e843a2ec44
Matheuslpss <matheuslopesmtb@gmail.com>
2025-01-15 11:52:46 -0300

    Opção de saida

--- Cartório da EBAC.c
+++ Cartório da EBAC.c
@@ -131,6 +131,7 @@
 	printf("\t1 - Registrar nomes\n");
 	printf("\t2 - Consultar nomes\n");
 	printf("\t3 - Deletar nomes\n\n\n"); //Fim do menu
+	printf("\t4 - Sair do sistema\n\n");
 	printf("Opção: ");  //Fim do menu
 	
 	scanf("%d",&opcao); //Armazenando a escolha do usário
@@ -149,6 +150,11 @@
 		
 		case 3:
 	    deletar();
+		break;
+		
+		case 4:
+		printf("Obrigado por utilizar nosso sistema!\n");
+		return 0;
 		break;
 		
 		default:
